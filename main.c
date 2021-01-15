d1475e6d3fc96c87d67fe13385c4356d7a672801
Bastian <bastian.muratory@gmail.com>
2021-01-15 21:22:36 +0000

    working

--- main.c
+++ main.c
@@ -17,6 +17,15 @@ int main(void) {
 	fscanf(fichier,"%d",&total);
 	fclose(fichier);
 	//printf("Il y a %d images\n",total);
+  printf("[%d]\n",total);
+	char c;
+	c = getchar();
+	if(c!='\n'){
+		return EXIT_FAILURE;
+	}else{
+		printf("%c",c);
+	}
+
 
 	char** liste = (char**)malloc(total*sizeof(char*));
 	char** liste2 = (char**)malloc(total*sizeof(char*));
@@ -36,7 +45,7 @@ int main(void) {
 			differentes++;
 			strcpy(current,liste2[i]);
 		}
-		printf("[%d] %s\n",i,liste2[i]);
+		//printf("[%d] %s\n",i,liste2[i]);
 	}
 	
 
@@ -58,49 +67,20 @@ int main(void) {
 	}
 
 	for(int i=0;i<differentes;i++){
-		printf("[%d] %s = %d\n",i,nom[i],occurence[i]);
+		//printf("[%d] %s = %d\n",i,nom[i],occurence[i]);
+		printf("%d\n",occurence[i]);
 	}
-
-	char name_of_unit[50];
-	printf("Nom de l'unitée :");
-	scanf("%s",name_of_unit);
-	char chemin[100] = "result/";
-	strcat(chemin,name_of_unit);
-
-	// creation du dossier teminal
-	char commande1[100] = "mkdir ";
-	strcat(commande1,chemin);
-	system(commande1);
-	strcat(commande1,"/sprites");
-	system(commande1);
-
-	// creation du fichier sprite
-	char commande2[100] = "touch ";
-	strcat(commande2,chemin);
-	strcat(commande2,"/sprite.txt");
-	system(commande2);
 
 	// ouverture du fichier sprite
-	char file[100] = "";
-	strcat(file,chemin);
-	strcat(file,"/sprite.txt");
-	fichier = fopen(file,"w");
-	fprintf(fichier,"%d,%d\n",total,differentes);
+	fichier = fopen("f","w");
 	for(int i=0;i<differentes;i++){
-		fprintf(fichier,"%d,%s\n",occurence[i],nom[i]);
+		fprintf(fichier,"%d\n",occurence[i]);
 	}
+
+	system("rm -rf unit");puts("");
+	system("mkdir unit");
 	fclose(fichier);
 	
-	char deplacement2[10] = "mv unit/";
-	for(int i = 0;i<total;i++){
-		char com[100] = "";
-		strcat(com,deplacement2);
-		strcat(com,liste[i]);
-		strcat(com," result/");
-		strcat(com,name_of_unit);
-		strcat(com,"/sprites");
-		system(com);
-	}
 
 
 
